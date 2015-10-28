aec09b22e49d92087bdaca06fcbb92f7a4aa17cb
Sam Wander <samwander@fb.com>
2015-10-28 12:01:56 -0700

    Paging indicator, fading in button in welcome screens

--- Carousel Proto/WelcomeViewController.swift
+++ Carousel Proto/WelcomeViewController.swift
@@ -8,16 +8,36 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
+class WelcomeViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var welcomeScrollView: UIScrollView!
     @IBOutlet weak var welcomeButtonImage: UIImageView!
     @IBOutlet weak var welcomeButton: UIButton!
     
+    @IBOutlet weak var proceedContainer: UIView!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         welcomeScrollView.contentSize = CGSize(width: 1280, height: 1.0)
+        welcomeScrollView.delegate = self
+        proceedContainer.alpha = 0
+    }
+    
+    // Fade out paging indicator, fade in button
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        var scrollX = Int(round(scrollView.contentOffset.x))
+        if scrollX > 640 {
+            pageControl.alpha = convertValue(CGFloat(scrollX), r1Min: 640.00, r1Max: 860.00, r2Min: 1.0, r2Max: 0.0)
+            proceedContainer.alpha = convertValue(CGFloat(scrollX), r1Min: 860.00, r1Max: 960.00, r2Min: 0.0, r2Max: 1.0)
+        }
+    }
+    
+    // Paging control
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        var page = Int(round(scrollView.contentOffset.x / 320))
+        pageControl.currentPage = page
     }
 
     override func didReceiveMemoryWarning() {
