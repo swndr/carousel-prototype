f3b27415bbb533477109e935a6cbab0352ab6b6a
Sam Wander <samwander@fb.com>
2015-10-30 22:19:08 -0700

    Select an image in feed, change status bar, load black bg, get image pos relative to window. Todo: place image in front of bg and animate.

--- Carousel Proto/FeedViewController.swift
+++ Carousel Proto/FeedViewController.swift
@@ -13,9 +13,23 @@ class FeedViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var feedScrollView: UIScrollView!
     @IBOutlet weak var scrubberScrollView: UIScrollView!
     
+    @IBOutlet weak var blackBackgroundImageView: UIImageView!
+    var currentBackgroundColor = "light"
+    
+    @IBOutlet weak var selectablePhoto: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        blackBackgroundImageView.alpha = 0
+        
+        let tapPhoto: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "didTapPhoto:")
+        selectablePhoto.userInteractionEnabled = true
+        selectablePhoto.addGestureRecognizer(tapPhoto)
+        
+        preferredStatusBarStyle()
+        setNeedsStatusBarAppearanceUpdate()
+        
         feedScrollView.contentSize = CGSize(width: 320, height: 1628)
         scrubberScrollView.contentSize = CGSize(width: 840, height: 36)
         feedScrollView.contentOffset.y = 1060
@@ -33,6 +47,28 @@ class FeedViewController: UIViewController, UIScrollViewDelegate {
         } else {
             let scrubOffset = convertValue(scrollView.contentOffset.y, r1Min: 0, r1Max: 1060, r2Min: 0, r2Max: 520)
             scrubberScrollView.contentOffset.x = scrubOffset
+        }
+    }
+    
+    func didTapPhoto(tap:UITapGestureRecognizer) {
+        currentBackgroundColor = "dark"
+        preferredStatusBarStyle()
+        setNeedsStatusBarAppearanceUpdate()
+        UIView.animateWithDuration(0.5) { () -> Void in
+            self.blackBackgroundImageView.alpha = 1
+        }
+        
+        // Getting the image position relative to window
+        let imagePos: CGPoint = self.selectablePhoto.convertPoint(self.selectablePhoto.frame.origin, toView: self.view)
+        print(imagePos)
+    }
+    
+    
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        switch currentBackgroundColor {
+        case "dark": return .LightContent
+        default: return .Default
         }
     }
     
